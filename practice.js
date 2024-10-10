4aa8c8fe43b70c08d086d05dbc71a8504886b628
VahidSobhanieh <sobhanieh.v@gmail.com>
2024-10-10 14:32:42 -0400

    Delete practice.js

--- practice.js
+++
@@ -1,9 +0,0 @@
-let cartQuantity = document.getElementsByClassName("Show quantity");
-cartQuantity = 0;
-let addToCart = document.getElementsByClassName("Add");
-addToCart.addEventListener("click", function () {
-  console.log(`Quantity is: ${cartQuantity}`);
-});
-
-console.log(`Quantity is: ${cartQuantity}`);
-console.log(`Quantity is: ${addToCart}`);
